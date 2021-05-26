e4577b8257fc24fccea017069217706a3013d1ab
BENJAMIN IDEWOR <benjamin.idewor@decagon.dev>
2021-05-26 23:25:14 +0100

    Added styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube';
+import VideoList from './VideoLIst';
 
 
 class App extends Component {
-    onSearchTermSubmit = (searchTerm) => {
-        youtube.get('/search',{
+    state = { videos: [] };
+
+    onSearchTermSubmit = async searchTerm => {
+        const response = await youtube.get('/search',{
             params: {
                 q: searchTerm
             }
         });
+
+        this.setState( { videos: response.data.items })
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onSearchTermSubmit} />
+                <VideoList videos={this.state.videos}/>
             </div>
         )
     }
